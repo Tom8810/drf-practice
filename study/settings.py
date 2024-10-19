0be2f0ec029280a4627ddc6a798847e809e07a66
Tomsoya8810 <soyan8810@gmail.com>
2024-10-19 09:47:06 +0900

    add requirements.txt

--- study/settings.py
+++ study/settings.py
@@ -36,7 +36,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
